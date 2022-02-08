2aa4631d63d01d5f00886631f90db4c52d180fb7
Michael Primavera <pastasauce59@gmail.com>
2022-02-07 22:58:53 -0500

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ export default class App extends Component {
   };
 
   componentDidMount() {
-    fetch("https://thawing-tor-30862.herokuapp.com/hotels")
+    fetch("http://127.0.0.1:3000/hotels")
       .then((res) => res.json())
       .then((hotelsArr) => this.setState({ hotels: hotelsArr }));
   }
