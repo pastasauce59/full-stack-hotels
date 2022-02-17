5a696a902f8167d24e9b7272459584396eb99d09
Michael Primavera <pastasauce59@gmail.com>
2022-02-16 23:59:29 -0500

    adjustments to styling

--- client/src/App.js
+++ client/src/App.js
@@ -150,6 +150,7 @@ export default class App extends Component {
           </div>
         </Switch>
       </Router>
+
     );
   }
 }
--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -4,12 +4,14 @@ import CartCard from "./CartCard";
 class Cart extends Component {
   render() {
     return (
-      <div className="cart-father">
+      <div>
+        <div className="cart-message">
          {this.props.resvItems.length > 0 ? (
             <h3>Your bookings:</h3>
           ) : (
             <h3 className="empty-cart">Please add to your cart</h3>
           )}
+          
 
           <button
             disabled={this.props.resvItems.length > 0 ? false : true}
@@ -17,6 +19,7 @@ class Cart extends Component {
             onClick={this.props.handleResvPost} >
             Checkout
           </button>
+        </div>
 
         <div className="cart-container">
           {this.props.resvItems.map((resv) => (
